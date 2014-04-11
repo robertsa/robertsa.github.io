82ab76ec76267d4cf751fa5792218f5fc24da376
Andy Roberts <blackcadillax@gmail.com>
2014-04-11 15:12:27 -0400

    Update content and styles

---
+++ script.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  
+  $( $(".project")[0] ).on("click", function() {
+    window.open("http://robertsa.github.io/blackjack");
+  });
+  
+  $( $(".project")[1] ).on("click", function() {
+    window.open("http://robertsa.github.io/memory");
+  });
+  
+  $( $(".project")[2] ).on("click", function() {
+    window.open("http://robertsa.github.io/colorboxes");
+  });
+  
+  $( $(".project")[3] ).on("click", function() {
+    window.open("http://robertsa.github.io/typewriter");
+  });
+  
+  $( $(".project")[4] ).on("click", function() {
+    window.open("http://robertsa.github.io/boxart");
+  });
+  
+});
